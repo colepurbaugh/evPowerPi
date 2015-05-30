9ad473ff6d3eeb3752e5390091491bea8cf8141b
colepurbaugh <colepurbaugh@gmail.com>
2015-05-30 23:54:20 +0000

    sample code go.js updates to remote database

--- go.js
+++ go.js
@@ -1,40 +1,55 @@
 var parseString = require('xml2js').parseString;
 var http = require('http');
 
-var objectEx = {
-	property1: 'text',
-	property2: 4
-}
-
 var options = {
 	host: '169.237.123.39',
 	port: '8080',
 	path: '/WebService_PV_Power/PV_Power'
 };
 
-var temporary;
+var plug = 1;
+var power = 1.2;
 
-callback = function(response) {
-	var str = '';
+var toDatabase = function (plug, power){
+	options.path = '/WebService_PV_Power/PV_Power/' + plug + '/' + power
 
-	//another chunk of data has been recieved, so append it to `str`
-	response.on('data', function (chunk) {
-		str += chunk;
-	});
-
-	//the whole response has been recieved, so we just print it out here
-	response.on('end', function () {
-		
-		parseString(str, function (err, result) {
-			console.log(objectEx);
-			console.log('\n\r*********************\n\r');
-			console.log(result);
-			console.log('\n\r*********************\n\r');
-			console.log(result.Response.Terminal);
-			
+	http.request(options, function(response) {
+		var str = '';
+	
+		//another chunk of data has been recieved, so append it to `str`
+		response.on('data', function (chunk) {
+			str += chunk;
 		});
+	
+		//the whole response has been recieved, so we just print it out here
+		response.on('end', function () {
+			
+			parseString(str, function (err, result) {
+				//Error
+				console.log('error = ' + result.Response.Terminal[0].Value[0]);
+				//EV_Power_actual
+				console.log('EVpower = ' + result.Response.Terminal[1].Value[0]);
+				//EV_Plugged
+				console.log('plugState = ' + result.Response.Terminal[2].Value[0]);
+				//PV_Power_kW
+				console.log('genPower = ' + result.Response.Terminal[3].Value[0]);
+			});
+	
+		});
+	}).end();
+};
 
-	});
-}
 
-http.request(options, callback).end();
+setInterval( function () {
+	toDatabase(plug, power);
+	if (plug < 5) {
+		plug++
+	}else {
+		plug = 0;
+	}
+	if (power < 20) {
+		power += 0.1
+	} else {
+		power = 0
+	}
+},1000);
